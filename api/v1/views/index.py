afbf80c59a3b655cad7acfd9fcbe0353729a6042
johnahumanbeing <mseewak@gmail.com>
2024-02-24 00:39:20 +0300

    index.py fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,10 @@
 """blueprint routes"""
 
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route("/status", strict_slashes=False, methods=["GET"])
-def status():
-    return {
-        "status": "OK",
-    }
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Display the status of the API"""
+    return jsonify({"status": "OK"})
