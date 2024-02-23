a392ebb3ee7858f94fde9a31f9c579533d6d64bf
johnahumanbeing <mseewak@gmail.com>
2024-02-24 00:35:07 +0300

    app.py fix1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,36 @@
-#!/usr/bin/pyhton3
-"""Route to return the API status"""
-import os
-from flask import Flask
+#!/usr/bin/python3
+"""start a flask application on 0.0.0.0 port 5000"""
+
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+import json
 from flask_cors import CORS
 
 
-# creating a Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close(ctx):
+def teardown(ret):
+    """Calls storage.close method"""
     storage.close()
 
 
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
-else:
-    host = "0.0.0.0"
-
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
-else:
-    port = 5000
+@app.errorhandler(404)
+def notFoundHandler(err):
+    """To handler the error 404 :Not found"""
+    error_msg = {"error": "Not found"}
+    response = make_response(json.dumps(error_msg), 404)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 
 if __name__ == "__main__":
+    """start application on 0.0.0.0:5000"""
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
