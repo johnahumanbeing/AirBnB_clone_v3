b06787ff6f2e713bc22d66edddfc81e764d441a4
johnahumanbeing <mseewak@gmail.com>
2024-02-24 00:44:14 +0300

    index.py fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,25 @@ from flask import jsonify
 def get_status():
     """Display the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """Display the stats"""
+    from models.user import User
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.state import State
+    from models.review import Review
+    from models import storage
+
+    stats_data = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return stats_data
