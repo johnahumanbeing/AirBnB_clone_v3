02d82867ce92cae993fbf7c61b76b9e2ea5dd7f0
johnahumanbeing <mseewak@gmail.com>
2024-02-24 00:37:14 +0300

    init file fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,7 @@
 """blueprint for app_views"""
 
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__)
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
